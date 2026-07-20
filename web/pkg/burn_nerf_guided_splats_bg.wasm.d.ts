a60456742241a577bca1de05ae1b6ea6fc0899f8
Kerwin Huang <qwerzxcv98@gmail.com>
2026-07-20 21:48:57 +0800

    build: compile WASM package for distribution

---
+++ web/pkg/burn_nerf_guided_splats_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmtrainingsession_free: (a: number, b: number) => void;
+export const wasmtrainingsession_get_gaussian_render: (a: number) => [number, number];
+export const wasmtrainingsession_get_nerf_importance_map: (a: number) => [number, number];
+export const wasmtrainingsession_get_nerf_render: (a: number) => [number, number];
+export const wasmtrainingsession_new: (a: number, b: number, c: number, d: number, e: number) => number;
+export const wasmtrainingsession_seed_from_nerf: (a: number) => void;
+export const wasmtrainingsession_step_gaussian: (a: number, b: number) => number;
+export const wasmtrainingsession_step_nerf: (a: number, b: number) => number;
+export const init_panic_hook: () => void;
+export const wasm_bindgen__convert__closures_____invoke__ha47ab804cced2a29: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h053ce51d358c81af: (a: number, b: number, c: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
